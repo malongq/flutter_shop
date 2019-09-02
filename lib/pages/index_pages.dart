0e647482a3a6d15329670d755c6ca35ee57dbe1a
马龙 <malong@100tal.com>
2019-09-02 11:21:08 +0800

    制作底部导航栏

--- lib/pages/index_pages.dart
+++ lib/pages/index_pages.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-class IndexPage extends StatelessWidget {
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+
+  final List<BottomNavigationBarItem> b = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页')
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.search),
+        title: Text('分类')
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.shopping_cart),
+        title: Text('购物车')
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.profile_circled),
+        title: Text('会员中心')
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('百姓生活+'),),
+      appBar: AppBar(title: Text('马龙'),),
       body: Center(
-        child: Text('百姓生活+'),
+        child: Text('哈哈'),
       ),
+      bottomNavigationBar: BottomNavigationBar(items: b,backgroundColor: Colors.pink,),
     );
   }
 }
+
+//TODO 只是测试一下
+//class IndexPage extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(title: Text('百姓生活+'),),
+//      body: Center(
+//        child: Text('百姓生活+'),
+//      ),
+//    );
+//  }
+//}
