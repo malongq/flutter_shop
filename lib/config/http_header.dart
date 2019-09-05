c5208b6fc98123dd7a6b9562ccd624fb636c9074
马龙 <malong@100tal.com>
2019-09-05 11:43:56 +0800

    模拟请求头

---
+++ lib/config/http_header.dart
@@ -0,0 +1,14 @@
+const http_header = {
+  'Accept': 'application/json, text/plain, */*',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Accept-Language': 'zh-CN,zh;q=0.9',
+  'Connection': 'keep-alive',
+  'Content-Length': '0',
+  'Cookie': 'Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1567651851; _ga=GA1.2.788211843.1567651851; _gid=GA1.2.1761900379.1567651851; MEIQIA_TRACK_ID=1QOqBRuEtUgz3hNCF5qB1oumblL; MEIQIA_PANEL_VISIBILITY=hidden; SERVERID=1fa1f330efedec1559b3abbcb6e30f50|1567651928|1567651850; _gat=1; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1567651929',
+  'Host': 'time.geekbang.org',
+  'Origin': 'https://time.geekbang.org',
+  'Referer': 'https://time.geekbang.org/',
+  'Sec-Fetch-Mode': 'cors',
+  'Sec-Fetch-Site': 'same-origin',
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import '../config/http_header.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,66 +9,48 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  TextEditingController _controller = TextEditingController();
-  String showText = '小伙子，现在你要开始起飞了 ~ ~';
+  var showText = '还没开始请求';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('动态请求后台替换页面'),),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(10.0),
-                labelText: '想要啥样的小姐类型？',
-                helperText: '来吧，别害羞，填上你喜欢的类型'
+
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(title: Text('伪造请求头'),),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: _requestJiKe,
+                child: Text('开始请求'),
               ),
-              autofocus: false,
-            ),
-            RaisedButton(
-              onPressed: _clickAction,
-              child: Text('选择完毕，go!!!'),
-            ),
-            Text(
-              showText,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 1,
-            )
-          ],
+              Text(showText)
+            ],
+          ),
         ),
       ),
     );
   }
 
-  //点击请求时判断逻辑
-  void _clickAction(){
-    print('开始选择你要的小姐');
-    if(_controller.text.toString()==''){
-      showDialog(
-        context: context,
-        builder: (context)=>AlertDialog(title: Text('别不选啊，小伙子'),)
-      );
-    }else{
-      getRequest(_controller.text.toString()).then((onValue){
-        setState(() {
-          showText=onValue['data']['name'].toString();
-        });
+  void _requestJiKe(){
+    print('准备请求');
+    getHttp().then((val){
+      setState(() {
+        showText = val['data'].toString();
       });
-    }
+    });
   }
 
-  //网络请求开始
-  Future getRequest(String inPutName) async{
+  Future getHttp() async{
     try{
       Response response;
-      var data = {'name':inPutName};
-      response = await Dio().get('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian',queryParameters: data);
+      Dio dio = new Dio();
+      dio.options.headers = http_header;
+      response = await dio.post('https://time.geekbang.org/serv/v1/column/topList');
+      print(response.data);
       return response.data;
     }catch(e){
-      return print(e);
+      print(e);
     }
   }
 
@@ -88,9 +71,102 @@ class _HomePageState extends State<HomePage> {
 
 
 
-//import 'package:flutter/material.dart';
-//import 'package:dio/dio.dart';
+
+
+
+
+//class HomePage extends StatefulWidget {
+//  @override
+//  _HomePageState createState() => _HomePageState();
+//}
 //
+//class _HomePageState extends State<HomePage> {
+//
+//  TextEditingController _controller = TextEditingController();
+//  String showText = '小伙子，现在你要开始起飞了 ~ ~';
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Container(
+//      child: Scaffold(
+//        appBar: AppBar(title: Text('动态请求后台替换页面')),
+//        body: SingleChildScrollView(
+//          child: Container(
+//            child: Column(
+//              children: <Widget>[
+//                TextField(
+//                  controller: _controller,
+//                  decoration: InputDecoration(
+//                      contentPadding: EdgeInsets.all(10.0),
+//                      labelText: '想要啥样的小姐类型？',
+//                      helperText: '来吧，别害羞，填上你喜欢的类型'
+//                  ),
+//                  autofocus: false,
+//                ),
+//                RaisedButton(
+//                  onPressed: _clickAction,
+//                  child: Text('选择完毕，go!!!'),
+//                ),
+//                Text(
+//                  showText,
+//                  overflow: TextOverflow.ellipsis,
+//                  maxLines: 1,
+//                )
+//              ],
+//            ),
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+//
+//  //点击请求时判断逻辑
+//  void _clickAction(){
+//    print('开始选择你要的小姐');
+//    if(_controller.text.toString()==''){
+//      showDialog(
+//        context: context,
+//        builder: (context)=>AlertDialog(title: Text('别不选啊，小伙子'),)
+//      );
+//    }else{
+//      getRequest(_controller.text.toString()).then((onValue){
+//        setState(() {
+//          showText=onValue['data']['name'].toString();
+//        });
+//      });
+//    }
+//  }
+//
+//  //网络请求开始
+//  Future getRequest(String inPutName) async{
+//    try{
+//      Response response;
+//      var name = {'name':inPutName};
+////      response = await Dio().get('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian',queryParameters: name);
+//      response = await Dio().post('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/post_abaojian',queryParameters: name);
+//      return response.data;
+//    }catch(e){
+//      return print(e);
+//    }
+//  }
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //class HomePage extends StatelessWidget {
 //  @override
 //  Widget build(BuildContext context) {
