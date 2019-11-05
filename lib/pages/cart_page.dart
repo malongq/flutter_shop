688c48b9ddb0e5d4b2a0da4a00d7f162955ed09e
马龙 <malong@100tal.com>
2019-11-05 23:03:58 +0800

    购物车逻辑再次补充

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,90 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provide/provide.dart';
+import '../provide/cart.dart';
 
 //todo 购物车页面
-class CartPage extends StatefulWidget {
-  @override
-  _CartPageState createState() => _CartPageState();
-}
-
-class _CartPageState extends State<CartPage> {
-  List<String> list = [];
-
+class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    _show();
-
     return Scaffold(
-      appBar: AppBar(title: Text('分类')),
-      body: Container(
-        child: ListView(
-          children: <Widget>[
-
-            Container(
-                height: 400,
-                color: Colors.teal,
-                child: ListView.builder(
-                  itemCount: list.length,
-                  itemBuilder: (context,index){
-                    return ListTile(
-                      title: Text(list[index]),
-                    );
-                  },
-                )
-            ),
-
-            RaisedButton(
-              color: Colors.red,
-              onPressed: (){
-                _add();
+      appBar: AppBar(title: Text('购物车'),),
+      body: FutureBuilder(
+        future: _getCartInfo(context),
+        builder: (context,snapshot){
+          if(snapshot.hasData){
+            List infoList = Provide.value<CartProvide>(context).cartInfo;
+            return ListView.builder(
+              itemCount: infoList.length,
+              itemBuilder: (context,index){
+                return ListTile(
+                  title: Text('名称：  ${infoList[index].goodsName}'),
+                  subtitle: Text('数量：${infoList[index].count}'),
+                );
               },
-              child: Text('增加'),
-            ),
-
-            RaisedButton(
-              color: Colors.green,
-              onPressed: (){
-                _clear();
-              },
-              child: Text('清空'),
-            ),
-
-          ],
-        ),
+            );
+          }else{
+            return Text('暂无数据');
+          }
+        },
       ),
     );
   }
 
-  //添加
-  void _add()async{
-    SharedPreferences sp1 = await SharedPreferences.getInstance();
-    String ml = '大家好，我是最靓的仔';
-    list.add(ml);
-    sp1.setStringList("mlong", list);
-    _show();
+  //查询购物车内添加的商品明细
+  Future<String> _getCartInfo(BuildContext context)async{
+    await Provide.value<CartProvide>(context).getCartInfo();
+    return 'end';
   }
 
-  //展示
-  void _show()async{
-    SharedPreferences sp2 = await SharedPreferences.getInstance();
-    if(sp2.getStringList('mlong') != null){
-      setState((){
-        list = sp2.getStringList('mlong');
-      });
-    }
-  }
-
-  //清空
-  void _clear()async{
-    SharedPreferences sp3 = await SharedPreferences.getInstance();
-    //sp3.clear();
-    sp3.remove('mlong');
-    setState((){
-      list = [];
-    });
-  }
 }
+
 
 
 
--- lib/provide/cart.dart
+++ lib/provide/cart.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
+import 'package:flutter_shop/model/cart_info.dart';
 
 //todo 详情页购物车添加删除逻辑类
 class CartProvide with ChangeNotifier{
   String cartString  = '[]';
+  List<CartInfoModel> cartInfo = [];
 
   save (goodsId,goodsName,count,price,images)async{
     SharedPreferences sp = await SharedPreferences.getInstance();
@@ -16,23 +18,27 @@ class CartProvide with ChangeNotifier{
     tempList.forEach((item){
       if(item['goodsId']==goodsId){
         tempList[i]['count'] = item['count'] + 1;
+        cartInfo[i].count++;
         isHave = true;
       }
       i++;
     });
 
     if(!isHave){
-      tempList.add({
+      Map<String,dynamic> newGoods={
         'goodsId' : goodsId,
         'goodsName' : goodsName,
         'count' : count,
         'price' : price,
         'images' : images,
-      });
+      };
+      tempList.add(newGoods);
+      cartInfo.add(CartInfoModel.fromJson(newGoods));
     }
 
     cartString = json.encode(tempList).toString();
-    print(cartString);
+    print('字符串：  ${cartString}');
+    print('集合： ${cartInfo}');
     sp.setString('cart_info', cartString);
     notifyListeners();
 
@@ -41,7 +47,23 @@ class CartProvide with ChangeNotifier{
   remove()async{
     SharedPreferences sp = await SharedPreferences.getInstance();
     sp.remove('cart_info');
+    cartInfo = [];
     print('清空完成。。。。。。。。。。。');
+    notifyListeners();
+  }
+
+  getCartInfo()async{
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    cartString = sp.getString('cart_info');
+    cartInfo = [];
+    if(cartString == null){
+      cartInfo = [];
+    }else{
+      List<Map> tempList = (json.decode(cartString.toString())as List).cast();
+      tempList.forEach((item){
+        cartInfo.add(CartInfoModel.fromJson(item));
+      });
+    }
     notifyListeners();
   }
 
